4bb6fcbae3c70370ee8816663ddc59c106bf65e2
Neekon <neekon91@gmail.com>
2016-12-04 17:29:09 -0800

    server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('client/public'));
+app.use(bodyParser.json());
+
+
+
+app.listen(3000, () => {
+  console.log('Express server is up on port 3000');
+});
+
+module.exports.app = app;
